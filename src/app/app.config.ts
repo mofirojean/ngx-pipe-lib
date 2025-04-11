a248b3cd1668ce2f87f730c70dab01f7ba9a2b7c
mofirojean <mofirojean@gmail.com>
2025-04-11 10:57:48 +0100

    feat: implement icon feature

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import * as icons from '@ng-icons/lucide';
+import { provideIcons } from '@ng-icons/core';
+
+
 
 import { routes } from './app.routes';
+import { provideHlmIconConfig } from '@spartan-ng/ui-icon-helm';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideIcons(icons)
+  ]
 };
--- src/app/components/code-preview/code-preview.component.ts
+++ src/app/components/code-preview/code-preview.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
 import {HlmButtonDirective} from '@spartan-ng/ui-button-helm';
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
 
 @Component({
   selector: 'app-code-preview',
-  imports: [HlmButtonDirective],
+  imports: [HlmButtonDirective, NgIcon, HlmIconDirective],
   templateUrl: './code-preview.component.html',
-  styles: ``
+  styles: `
+    .dot {
+      position: absolute;
+      inset: 0;
+      height: 100%;
+      width: 100%;
+      background-image: radial-gradient(#e5e7eb 2px, transparent 2px);
+      background-size: 16px 16px;
+    }
+  `
 })
 export class CodePreviewComponent {
 
