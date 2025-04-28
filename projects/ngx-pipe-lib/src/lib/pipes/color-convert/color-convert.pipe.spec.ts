9f4f3990f37a81516628e6855d03830d1d990c4b
mofirojean <mofirojean@gmail.com>
2025-04-28 22:01:10 +0100

    mod: add test for color convert pipe

--- projects/ngx-pipe-lib/src/lib/pipes/color-convert/color-convert.pipe.spec.ts
+++ projects/ngx-pipe-lib/src/lib/pipes/color-convert/color-convert.pipe.spec.ts
@@ -5,9 +5,21 @@ describe('ColorConvertPipe', () => {
 
   beforeEach(() => {
     pipe = new ColorConvertPipe();
-  })
+  });
 
   it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should convert HEX to RGB', () => {
+    expect(pipe.transform('#FF0000', 'rgb')).toBe('rgb(255, 0, 0)');
+  });
+
+  it('should convert RGB to HEX', () => {
+    expect(pipe.transform('rgb(0, 255, 0)', 'hex')).toBe('#00FF00');
+  });
+
+  it('should return input for invalid format', () => {
+    expect(pipe.transform('invalid', 'rgb')).toBe('invalid');
   });
 });
